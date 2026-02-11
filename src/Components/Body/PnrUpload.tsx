b4136ee43469a5b79e45af8a96757e7c4a73817e
Mwaura Lewis <lewmonk@gmail.com>
2026-02-11 09:53:28 +0300

    Update PRN Generation

--- src/Components/Body/PnrUpload.tsx
+++ src/Components/Body/PnrUpload.tsx
@@ -16,221 +16,222 @@ import ConfirmDetails from "../pnr/ConfirmDetails";
 import Revenue from "../pnr/Revenue";
 
 export default function PnrUpload() {
-  const [loading, setLoading] = useState<boolean>(false);
-  const [gridApi, setGridApi] = useState(null);
-  const [date, setDate] = useState<string>();
-  const [modalIsOpen, setIsOpen] = useState(false);
-  const [dataTable, setDataTable] = useState<string>("stakes");
+	const [loading, setLoading] = useState<boolean>(false);
+	const [gridApi, setGridApi] = useState(null);
+	const [date, setDate] = useState<string>();
+	const [modalIsOpen, setIsOpen] = useState(false);
+	const [dataTable, setDataTable] = useState<string>("stakes");
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const notify = (msg: string) => toast(msg);
-  const onsubmit = (data: any) => {
-    setLoading(true);
-    handlePnrUpload(data)
-      .then((res) => {
-        setLoading(false);
-        notify("Report added successfully");
-      })
-      .catch((err: any) => {
-        setLoading(false);
-        notify("Error while uploading PNR" + " " + err.message);
-      });
-  };
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+	const notify = (msg: string) => toast(msg);
+	const onsubmit = (data: any) => {
+		setLoading(true);
+		handlePnrUpload(data)
+			.then((res) => {
+				setLoading(false);
+				notify("Report added successfully");
+			})
+			.catch((err: any) => {
+				setLoading(false);
+				notify("Error while uploading PNR" + " " + err.message);
+			});
+	};
 
-  const { data, isLoading, isError } = useQuery<TaxInfo>(
-    [`prn status ${date}`],
-    async () => await handleGetPnrStatus(date),
-    {
-      cacheTime: 86400,
-      staleTime: 86400 / 24,
-    }
-  );
+	const { data, isLoading, isError } = useQuery<TaxInfo>(
+		[`prn status ${date}`],
+		async () => await handleGetPnrStatus(date),
+		{
+			cacheTime: 86400,
+			staleTime: 86400 / 24,
+		}
+	);
+	console.log(data);
 
-  if (isLoading) {
-    return (
-      <div className="h-full w-full flex items-center justify-center">
-        <Loader />
-      </div>
-    );
-  }
+	if (isLoading) {
+		return (
+			<div className="h-full w-full flex items-center justify-center">
+				<Loader />
+			</div>
+		);
+	}
 
-  const onBtnExport = () => {
-    //@ts-ignore: Object is possibly 'null'
-    gridApi.exportDataAsCsv();
-  };
+	const onBtnExport = () => {
+		//@ts-ignore: Object is possibly 'null'
+		gridApi.exportDataAsCsv();
+	};
 
-  const onGridReady = (params: any) => {
-    setGridApi(params.api);
-  };
+	const onGridReady = (params: any) => {
+		setGridApi(params.api);
+	};
 
-  function openModal() {
-    setIsOpen(true);
-  }
+	function openModal() {
+		setIsOpen(true);
+	}
 
-  function closeModal() {
-    setIsOpen(false);
-  }
+	function closeModal() {
+		setIsOpen(false);
+	}
 
-  const customStyles = {
-    content: {
-      top: "50%",
-      height: "50%",
-      width: "35%",
-      left: "50%",
-      right: "auto",
-      bottom: "auto",
-      marginRight: "-50%",
-      transform: "translate(-50%, -50%)",
-    },
-    overlay: {
-      backgroundColor: "rgba(0,0,0,0.5)",
-    },
-  };
+	const customStyles = {
+		content: {
+			top: "50%",
+			height: "50%",
+			width: "35%",
+			left: "50%",
+			right: "auto",
+			bottom: "auto",
+			marginRight: "-50%",
+			transform: "translate(-50%, -50%)",
+		},
+		overlay: {
+			backgroundColor: "rgba(0,0,0,0.5)",
+		},
+	};
 
-  return (
-    <div className="w-full h-full grid grid-cols-1 bg-[#F6F8FA] justify-items-center py-10 font-secondary">
-      <Modal isOpen={modalIsOpen} style={customStyles}>
-        <ConfirmDetails data={data!} close={closeModal} />
-      </Modal>
-      <div className="w-[97%] flex flex-col gap-8">
-        <div className="h-1/4 w-full px-2 py-2 flex flex-col bg-white rounded-md shadow-lg">
-          <div className="w-full flex justify-between  py-2">
-            <div className="flex gap-2 items-center">
-              <div className="h-16 w-16 bg-primary rounded-full flex items-center justify-center">
-                <img src={Money} alt="Money Paid" className="h-8 " />
-              </div>
-              <h1 className="text-xs grid grid-cols-1">
-                <span className="text-xl font-extrabold">
-                  {" "}
-                  KES. {data!.totalExcise + data!.totalWithholding}
-                </span>
-                Amount Payable
-              </h1>
-            </div>
-            <div>
-              {date == null || date == "" ? (
-                <input
-                  type="date"
-                  className="py-2 px-8 rounded bg-[#F6F8FA]"
-                  value={date}
-                  onChange={(e) =>
-                    setDate(moment(e.target.value).format("DD-MM-YYYY"))
-                  }
-                />
-              ) : (
-                <div className="flex gap-4 items-center font-bold">
-                  {date}
-                  <img
-                    src={Cross}
-                    alt="Clear"
-                    className="h-6 w-6 cursor-pointer"
-                    onClick={() => setDate(undefined)}
-                  />
-                </div>
-              )}
-            </div>
-          </div>
-          <div className="flex flex-1 items-center justify-between p">
-            <Revenue
-              amount={data!.totalExcise}
-              label="Total Exercise"
-              date={date}
-            />
-            <Revenue
-              amount={data!.totalWithholding}
-              label="Total WTH"
-              date={date}
-            />
-            <Revenue
-              amount={data!.totalPayouts}
-              label="Total PayOuts"
-              date={date}
-            />
-            <Revenue
-              amount={data!.totalWinnings}
-              label="Total Winning"
-              date={date}
-            />
-            <button
-              disabled={!date == null || date == ""}
-              onClick={openModal}
-              className={
-                date == null || date == ""
-                  ? "bg-gray-300 px-12 py-2 rounded cursor-not-allowed"
-                  : "bg-secondary px-12 py-2 rounded cursor-pointer text-white font-bold"
-              }
-            >
-              {" "}
-              Pay
-            </button>
-          </div>
-        </div>
-        <div>
-          <div className="pb-2 w-full flex justify-end">
-            <select
-              name="Data"
-              id=""
-              className="rounded py-2 px-4"
-              onChange={(e) => setDataTable(e.target.value)}
-            >
-              <option value="stakes">Stakes</option>
-              <option value="outcome">Outcome</option>
-            </select>
-          </div>
-          <div
-            className="h-full w-full ag-theme-alpine shadow-lg rounded-md"
-            style={{ height: 500, width: "100%" }}
-          >
-            {dataTable == "stakes" ? (
-              <AgGridReact
-                rowData={data!.stakes}
-                defaultColDef={{
-                  sortable: true,
-                  flex: 1,
-                  minWidth: 100,
-                  filter: true,
-                  resizable: true,
-                }}
-                pagination
-                paginationPageSize={9}
-                animateRows={true}
-                onGridReady={onGridReady}
-              >
-                <AgGridColumn field="betId" />
-                <AgGridColumn field="mobileNo" />
-                <AgGridColumn field="exciseAmt" />
-                <AgGridColumn field="stakeType" />
-                <AgGridColumn field="expectedOutcomeTime" />
-              </AgGridReact>
-            ) : (
-              <AgGridReact
-                rowData={data!.outcomes}
-                defaultColDef={{
-                  sortable: true,
-                  flex: 1,
-                  minWidth: 100,
-                  filter: true,
-                  resizable: true,
-                }}
-                pagination
-                paginationPageSize={9}
-                animateRows={true}
-                onGridReady={onGridReady}
-              >
-                <AgGridColumn field="betId" />
-                <AgGridColumn field="customerId" />
-                <AgGridColumn field="withholdingTax" />
-                <AgGridColumn field="outcome" />
-                <AgGridColumn field="outcomeDate" />
-              </AgGridReact>
-            )}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="w-full h-full grid grid-cols-1 bg-[#F6F8FA] justify-items-center py-10 font-secondary">
+			<Modal isOpen={modalIsOpen} style={customStyles}>
+				<ConfirmDetails data={data!} close={closeModal} />
+			</Modal>
+			<div className="w-[97%] flex flex-col gap-8">
+				<div className="h-1/4 w-full px-2 py-2 flex flex-col bg-white rounded-md shadow-lg">
+					<div className="w-full flex justify-between  py-2">
+						<div className="flex gap-2 items-center">
+							<div className="h-16 w-16 bg-primary rounded-full flex items-center justify-center">
+								<img src={Money} alt="Money Paid" className="h-8 " />
+							</div>
+							<h1 className="text-xs grid grid-cols-1">
+								<span className="text-xl font-extrabold">
+									{" "}
+									KES. {data!.totalExcise + data!.totalWithholding}
+								</span>
+								Amount Payable
+							</h1>
+						</div>
+						<div>
+							{date == null || date == "" ? (
+								<input
+									type="date"
+									className="py-2 px-8 rounded bg-[#F6F8FA]"
+									value={date}
+									onChange={(e) =>
+										setDate(moment(e.target.value).format("DD-MM-YYYY"))
+									}
+								/>
+							) : (
+								<div className="flex gap-4 items-center font-bold">
+									{date}
+									<img
+										src={Cross}
+										alt="Clear"
+										className="h-6 w-6 cursor-pointer"
+										onClick={() => setDate(undefined)}
+									/>
+								</div>
+							)}
+						</div>
+					</div>
+					<div className="flex flex-1 items-center justify-between p">
+						<Revenue
+							amount={data!.deposit.total_amount}
+							label="Total Deposits"
+							date={date}
+						/>
+						<Revenue
+							amount={data!.deposit.total_amount * 0.05}
+							label="Total Exercise"
+							date={date}
+						/>
+						<Revenue
+							amount={data!.withdrawal.total_amount}
+							label="Total Withdrawals"
+							date={date}
+						/>
+						<Revenue
+							amount={data!.withdrawal.total_amount * 0.05}
+							label="Total WTH"
+							date={date}
+						/>
+						<button
+							disabled={!date == null || date == ""}
+							onClick={openModal}
+							className={
+								date == null || date == ""
+									? "bg-gray-300 px-12 py-2 rounded cursor-not-allowed"
+									: "bg-secondary px-12 py-2 rounded cursor-pointer text-white font-bold"
+							}
+						>
+							{" "}
+							Pay
+						</button>
+					</div>
+				</div>
+				<div>
+					<div className="pb-2 w-full flex justify-end">
+						<select
+							name="Data"
+							id=""
+							className="rounded py-2 px-4"
+							onChange={(e) => setDataTable(e.target.value)}
+						>
+							<option value="stakes">Stakes</option>
+							<option value="outcome">Outcome</option>
+						</select>
+					</div>
+					<div
+						className="h-full w-full ag-theme-alpine shadow-lg rounded-md"
+						style={{ height: 500, width: "100%" }}
+					>
+						{dataTable == "stakes" ? (
+							<AgGridReact
+								rowData={data!.stakes}
+								defaultColDef={{
+									sortable: true,
+									flex: 1,
+									minWidth: 100,
+									filter: true,
+									resizable: true,
+								}}
+								pagination
+								paginationPageSize={9}
+								animateRows={true}
+								onGridReady={onGridReady}
+							>
+								<AgGridColumn field="betId" />
+								<AgGridColumn field="mobileNo" />
+								<AgGridColumn field="exciseAmt" />
+								<AgGridColumn field="stakeType" />
+								<AgGridColumn field="expectedOutcomeTime" />
+							</AgGridReact>
+						) : (
+							<AgGridReact
+								rowData={data!.outcomes}
+								defaultColDef={{
+									sortable: true,
+									flex: 1,
+									minWidth: 100,
+									filter: true,
+									resizable: true,
+								}}
+								pagination
+								paginationPageSize={9}
+								animateRows={true}
+								onGridReady={onGridReady}
+							>
+								<AgGridColumn field="betId" />
+								<AgGridColumn field="customerId" />
+								<AgGridColumn field="withholdingTax" />
+								<AgGridColumn field="outcome" />
+								<AgGridColumn field="outcomeDate" />
+							</AgGridReact>
+						)}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/Components/pnr/ConfirmDetails.tsx
+++ src/Components/pnr/ConfirmDetails.tsx
@@ -7,141 +7,142 @@ import Money from "../../assets/svg/iconmonstr-banknote-thin.svg";
 import Revenue from "./Revenue";
 
 interface Iprops {
-  data: TaxInfo;
-  close: () => void;
+	data: TaxInfo;
+	close: () => void;
 }
 export default function ConfirmDetails({ data, close }: Iprops) {
-  const [loading, setLoading] = useState<boolean>(false);
-  const [confirm, setConfirm] = useState<boolean>(false);
-  const [payExcise, setPayExcise] = useState<boolean>(true);
-  const [payWHT, setPayWHT] = useState<boolean>(true);
-  const [amount, setAmount] = useState<number>(
-    data.totalExcise + data.totalWithholding
-  );
+	const [loading, setLoading] = useState<boolean>(false);
+	const [confirm, setConfirm] = useState<boolean>(false);
+	const [payExcise, setPayExcise] = useState<boolean>(true);
+	const [payWHT, setPayWHT] = useState<boolean>(true);
+	const [amount, setAmount] = useState<number>(
+		parseFloat(data.deposit.total_amount * 0.05) + parseFloat(data.withdrawal.total_amount * 0.05)
+	);
+	console.log(amount);
 
-  const notify = (msg: string) => toast(msg);
+	const notify = (msg: string) => toast(msg);
 
-  const handlePay = async () => {
-    setLoading(true);
-    try {
-      let res = await handlePnrUpload({
-        forDate: data.periodTo,
-        payExcise: payExcise,
-        payWHT: payWHT,
-      });
+	const handlePay = async () => {
+		setLoading(true);
+		try {
+			let res = await handlePnrUpload({
+				forDate: data.periodTo,
+				payExcise: payExcise,
+				payWHT: payWHT,
+			});
 
-      if (res.data.success) {
-        setLoading(false);
-        notify("Payment Sent Successfully");
-        close();
-      }
-    } catch (error: any) {
-      setLoading(false);
-      notify("Error while sending payment" + " " + error.message);
-    }
-  };
-  return (
-    <div className="grid grid-cols-1 gap-8 justify-items-center">
-      <div className="border-b border-dashed border-gray-400 pb-8 w-full">
-        <div className="flex gap-2 items-center">
-          <div className="h-16 w-16 bg-red-300 rounded-full flex items-center justify-center">
-            <img src={Money} alt="Money Paid" className="h-8 " />
-          </div>
-          <h1 className="text-xs grid grid-cols-1 gap-1">
-            <span className="text-xl font-extrabold">
-              {" "}
-              KES. {amount.toFixed(4)}
-            </span>
-            <span className="text-xs text-gray-500">
-              Amount Payable from{" "}
-              {data.periodFrom ??
-                moment().subtract(1, "days").format("DD-MM-YYYY")}{" "}
-              to {data.periodTo ?? moment().format("DD-MM-YYYY")}
-            </span>
-          </h1>
-        </div>
-      </div>
-      <div className="flex gap-8 pb-8 border-b border-dashed border-gray-400 w-full">
-        <div className="flex items-start gap-3">
-          <input
-            type="checkbox"
-            name="payExcise"
-            id=""
-            checked={payExcise}
-            // onChange={() => {
-            //   setPayExcise(!payExcise);
-            //   setAmount(
-            //     payExcise == true
-            //       ? data.totalWithholding + data.totalExcise
-            //       : data.totalWithholding
-            //   );
-            // }}
-            className="h-6 mt-1 w-6 py-2 px-2 rounded"
-          />
-          <Revenue
-            amount={data.totalExcise}
-            label="Total Exercise"
-            date={
-              data.periodFrom ??
-              moment().subtract(1, "days").format("DD-MM-YYYY")
-            }
-          />
-        </div>
-        <div className="flex items-start gap-3">
-          <input
-            type="checkbox"
-            name="payWHT"
-            id=""
-            checked={payWHT}
-            // onChange={() => {
-            //   setPayWHT(!payWHT);
-            //   setAmount(
-            //     payWHT == true
-            //       ? data.totalWithholding + data.totalExcise
-            //       : data.totalExcise
-            //   );
-            // }}
-            className="h-6 w-6 py-2 mt-1 px-2 rounded"
-          />
-          <Revenue
-            amount={data.totalWithholding}
-            label="Total Withholding"
-            date={
-              data.periodFrom ??
-              moment().subtract(1, "days").format("DD-MM-YYYY")
-            }
-          />
-        </div>
-      </div>
-      <div className="flex items-center gap-2 w-full">
-        <input
-          type="checkbox"
-          name="confirm"
-          id=""
-          checked={confirm}
-          className="h-6 w-6 py-2 mt-1 px-2 rounded"
-          onChange={() => {
-            setConfirm(!confirm);
-          }}
-        />
-        <label htmlFor="confirm" className="text-xs text-gray-500">
-          I confirm that the above details are correct
-        </label>
-      </div>
-      <button
-        disabled={confirm && amount > 1}
-        onClick={handlePay}
-        className={
-          confirm && amount > 1
-            ? "bg-secondary px-12 py-2 w-40 rounded cursor-pointer text-white font-bold"
-            : " bg-gray-300 px-12 py-2 w-40 rounded cursor-not-allowed"
-        }
-      >
-        <span className="text-base text-white ">Pay</span>
-      </button>
-      <button className={" w-32 py-2  rounded"} onClick={close}>
-        <span className=" -mt-2 text-secondary">Cancel</span>
-      </button>
-    </div>
-  );
+			if (res.data.success) {
+				setLoading(false);
+				notify("Payment Sent Successfully");
+				close();
+			}
+		} catch (error: any) {
+			setLoading(false);
+			notify("Error while sending payment" + " " + error.message);
+		}
+	};
+	return (
+		<div className="grid grid-cols-1 gap-8 justify-items-center">
+			<div className="border-b border-dashed border-gray-400 pb-8 w-full">
+				<div className="flex gap-2 items-center">
+					<div className="h-16 w-16 bg-red-300 rounded-full flex items-center justify-center">
+						<img src={Money} alt="Money Paid" className="h-8 " />
+					</div>
+					<h1 className="text-xs grid grid-cols-1 gap-1">
+						<span className="text-xl font-extrabold">
+							{" "}
+							KES. {amount.toFixed(4)}
+						</span>
+						<span className="text-xs text-gray-500">
+							Amount Payable from{" "}
+							{data.periodFrom ??
+								moment().subtract(1, "days").format("DD-MM-YYYY")}{" "}
+							to {data.periodTo ?? moment().format("DD-MM-YYYY")}
+						</span>
+					</h1>
+				</div>
+			</div>
+			<div className="flex gap-8 pb-8 border-b border-dashed border-gray-400 w-full">
+				<div className="flex items-start gap-3">
+					<input
+						type="checkbox"
+						name="payExcise"
+						id=""
+						checked={payExcise}
+						// onChange={() => {
+						//   setPayExcise(!payExcise);
+						//   setAmount(
+						//     payExcise == true
+						//       ? data.totalWithholding + data.totalExcise
+						//       : data.totalWithholding
+						//   );
+						// }}
+						className="h-6 mt-1 w-6 py-2 px-2 rounded"
+					/>
+					<Revenue
+						amount={data.deposit.total_amount * 0.05}
+						label="Total Exercise"
+						date={
+							data.periodFrom ??
+							moment().subtract(1, "days").format("DD-MM-YYYY")
+						}
+					/>
+				</div>
+				<div className="flex items-start gap-3">
+					<input
+						type="checkbox"
+						name="payWHT"
+						id=""
+						checked={payWHT}
+						// onChange={() => {
+						//   setPayWHT(!payWHT);
+						//   setAmount(
+						//     payWHT == true
+						//       ? data.totalWithholding + data.totalExcise
+						//       : data.totalExcise
+						//   );
+						// }}
+						className="h-6 w-6 py-2 mt-1 px-2 rounded"
+					/>
+					<Revenue
+						amount={data.withdrawal.total_amount * 0.05}
+						label="Total Withholding"
+						date={
+							data.periodFrom ??
+							moment().subtract(1, "days").format("DD-MM-YYYY")
+						}
+					/>
+				</div>
+			</div>
+			<div className="flex items-center gap-2 w-full">
+				<input
+					type="checkbox"
+					name="confirm"
+					id=""
+					checked={confirm}
+					className="h-6 w-6 py-2 mt-1 px-2 rounded"
+					onChange={() => {
+						setConfirm(!confirm);
+					}}
+				/>
+				<label htmlFor="confirm" className="text-xs text-gray-500">
+					I confirm that the above details are correct
+				</label>
+			</div>
+			<button
+				disabled={confirm && amount > 1}
+				onClick={handlePay}
+				className={
+					confirm && amount > 1
+						? "bg-secondary px-12 py-2 w-40 rounded cursor-pointer text-white font-bold"
+						: " bg-gray-300 px-12 py-2 w-40 rounded cursor-not-allowed"
+				}
+			>
+				<span className="text-base text-white ">Pay</span>
+			</button>
+			<button className={" w-32 py-2  rounded"} onClick={close}>
+				<span className=" -mt-2 text-secondary">Cancel</span>
+			</button>
+		</div>
+	);
 }
